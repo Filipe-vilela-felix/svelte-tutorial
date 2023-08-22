8dd27d08ca04181a2fcb9771dea8b2bfc18c3aa3
Filipe Vilela <fvf0092000@gmail.com>
2023-08-22 12:27:04 -0300

    updating-arrays-object

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,29 @@
 <script>
-	let count = 0;
-  
-  $: console.log('this count is ${count}')
-  $: console.log('this will also be logged whenever count changes')
+  let numbers = [1, 2, 3, 4]
 
-  $: if(count >= 10) {
-		alert('count is dangeroulsly hight!');
-		count = 0
-	}
-
-  function increment() {
-    count += 1;
+  function addNumber() {
+    numbers = [...numbers, numbers.length + 1]
   }
+
+  $: sum = numbers.reduce((total, currentNumber) => total + currentNumber, 0)
 </script>
 
-<button on:click={increment}>
-  Clicked {count}
-  {count === 1 ? 'time' : 'times'}
+<p>{numbers.join(' + ')} = {sum}</p>
+
+<button on:click={addNumber}>
+  Add a number
 </button>
 
 <!--
 
- Não estamos limitados a declarar valores reativos — também podemos executar instruções arbitrárias reativamente.
- Assim também como colocar o $: na frente de blocos.
+  A reatividade do Svelte é acionada por atribuições, o que significa que ela é ativada quando um valor é atribuído a uma variável1. 
+  Isso significa que métodos de matriz que alteram o conteúdo de uma matriz, como push e pop, não acionarão automaticamente atualizações na página da web, 
+  pois eles não usam o operador de atribuição =1. Para acionar atualizações na página da web ao usar esses métodos,
+   você precisa usar uma atribuição adicional para atualizar o valor da variável que armazena a matriz. (linha 5);
+
+  Atribuições a propriedades de matrizes e objetos — por exemplo — funcionam da mesma forma que as atribuições aos próprios valores.
+    Ex: function addNumber() {
+          numbers[numbers.length] = numbers.length + 1;
+        }
 
 -->
