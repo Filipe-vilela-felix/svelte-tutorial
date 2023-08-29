a2bb637022257880c46154a5353cf735dc2b59a7
Filipe Vilela <fvf0092000@gmail.com>
2023-08-29 15:55:45 -0300

    checkbox-inputs

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,41 @@
 <script>
-  let a = 1;
-  let b = 2;
+  let yes = false;
 </script>
 
 <label>
-  <input type="number" bind:value={a} min="0" max="10" />
-  <input type="range" bind:value={a} min="0" max="10" />
+  <input type="checkbox" bind:checked={yes} />
+  Yes! Send me regular email span
 </label>
 
-<br>
+{#if yes}
+  <p>
+    Thank you. We will bombard your inbox and sell
+		your personal details.
+  </p>
+  {:else}
+  <p>
+    You must opt in to continue. If you're not
+		paying, you're the product.
+  </p>
+{/if}
 
-<label>
-  <input type="number" bind:value={b} min="0" max="10" />
-  <input type="range" bind:value={b} min="0" max="10" />
-</label>
-
-<br>
-
-{a} + {b} = {a + b} 
+<button disabled={!yes}>Subscribe</button>
 
 <!--
 
-  Claro! O DOM (Document Object Model) é uma representação em árvore da estrutura de uma página da web. 
-  No DOM, todos os valores são armazenados como strings, mesmo que sejam números. Isso pode ser um problema quando você está trabalhando com 
-  entradas numéricas, como campos de entrada do tipo “number” ou “range”, pois significa que você precisa lembrar de converter o valor da entrada 
-  para um número antes de usá-lo.
+  As caixas de seleção são usadas para alternar entre estados. Em vez de vincular a input.value, vinculamos a input.checked. (linha 6);
 
-  Com bind:value, o Svelte cuida disso para você. (linhas 7, 8,  14 e 15);
+  Contextualizando o código:
+    - A primeira parte do código define uma variável yes e atribui a ela o valor false. (linha 2);
+
+    - Em seguida, o código cria um elemento label que contém um elemento input do tipo checkbox. 
+      O estado da caixa de seleção é vinculado à variável yes usando a diretiva bind:checked. (linhas 5 a 8);
+        (Isso significa que, quando a caixa de seleção é marcada ou desmarcada pelo usuário, 
+        o valor da variável yes é atualizado para refletir o estado atual da caixa de seleção.)
+    
+    - A próxima parte do código usa uma estrutura condicional {#if ...}{:else}{/if} 
+      para exibir uma mensagem diferente dependendo do valor da variável yes. (linhas 10 a 20);
+    
+    - Por fim, o código cria um botão que é desativado quando a variável yes é falsa (ou seja, quando a caixa de seleção não está marcada). (linha 22);
+
 -->
