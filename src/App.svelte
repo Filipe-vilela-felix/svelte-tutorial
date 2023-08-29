d72622a5c35755a5e3942468974be823dfae07f0
Filipe Vilela <fvf0092000@gmail.com>
2023-08-29 14:55:57 -0300

    text-inputs

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,17 @@
 <script>
-	import BigRedButton from './BigRedButton.svelte';
-	import horn from './horn.mp3';
-
-	const audio = new Audio();
-	audio.src = horn;
-
-	function handleClick() {
-		audio.play();
-	}
+  let name = 'world';
 </script>
 
-<BigRedButton on:click={handleClick} />
+<input bind:value={name} />
+
+<h1>Hello {name}!</h1>
+
+<!--
+
+  Como regra geral, o fluxo de dados no Svelte é de cima para baixo — um componente pai pode definir props em um componente filho e 
+  um componente pode definir atributos em um elemento, mas não o contrário.
+
+  Às vezes é útil quebrar essa regra, utilizando a directiva: bind:value. (linha 5);
+  Note que usando essa propriedade, o valor do nome usado em <h1> é alteraldo após alteração do mesmo em <input/>.
+
+-->
