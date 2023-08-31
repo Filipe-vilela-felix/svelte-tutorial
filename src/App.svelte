4ab710b582da6446983a64a25aebeb650df91bde
Filipe Vilela <fvf0092000@gmail.com>
2023-08-31 17:50:29 -0300

    select-multiple

--- src/App.svelte
+++ src/App.svelte
@@ -22,18 +22,11 @@
 
 <h2>Flavours</h2>
 
-{#each ['cookies and cream', 'mint choc chip', 'raspberry ripple'] as flavour}
-  <label>
-    <input 
-      type="checkbox"
-      name="flavours"
-      value={flavour}
-      bind:group={flavours}
-    />
-    {flavour}
-    <br>
-  </label>
-{/each}
+<select multiple bind:value={flavours}>
+  {#each ['cookies and cream', 'mint choc chip', 'raspberry ripple'] as flavour}
+    <option>{flavour}</option>
+  {/each}
+</select>
 
 {#if flavours.length === 0} 
   <p>Please select at least one flavour</p>
@@ -47,6 +40,10 @@
 {/if}
 
 <!--
+
+  O elemento <select> pode ter um atributo multiple, que permite ao usuário selecionar várias opções ao mesmo tempo.
+
+  Quando o atributo multiple está presente, o valor do elemento <select> é uma matriz contendo os valores das opções selecionadas pelo usuário.
 
   Contextualizando o código:
     Este código permite ao usuário escolher o tamanho e os sabores de um sorvete.
@@ -63,9 +60,9 @@
         Para cada número na matriz, o código cria um elemento <label> contendo um elemento <input> do tipo “radio” e um texto que exibe
         o número de bolas de sorvete correspondente. O atributo value do elemento <input> é definido como o número atual da iteração.
       - Na seção “Sabores”:
-        O código usa outro loop {#each} para iterar sobre uma matriz de strings representando os sabores disponíveis. 
-        Para cada sabor na matriz, o código cria um elemento <label> contendo um elemento <input> do tipo “checkbox” e um texto que exibe 
-          o sabor correspondente. O atributo value do elemento <input> é definido como o sabor atual da iteração.
+        O código começa com um elemento <select> que tem o atributo multiple e a diretiva bind:value vinculando seu valor à variável flavours.
+        Dentro do elemento <select>, o código usa um loop {#each} para iterar sobre uma matriz de strings representando os sabores disponíveis. 
+        Para cada sabor na matriz, o código cria um elemento <option> contendo o sabor correspondente.
     
     - Finalmente, o código usa uma estrutura condicional {#if} para verificar se a matriz flavours está vazia ou se seu comprimento é maior que o valor da variável scoops. 
       Se a matriz estiver vazia, o código exibe uma mensagem pedindo ao usuário para selecionar pelo menos um sabor. 
