64b5def0407e45ad20788e48a17e860d382cea98
Filipe Vilela <fvf0092000@gmail.com>
2023-09-04 19:43:35 -0300

    custom-stores

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,32 @@
 <script>
-	import { time, elapsed } from './stores.js';
-
-	const formatter = new Intl.DateTimeFormat(
-		'en',
-		{
-			hour12: true,
-			hour: 'numeric',
-			minute: '2-digit',
-			second: '2-digit'
-		}
-	);
+    import { count } from "./stores";
 </script>
 
-<h1>The time is {formatter.format($time)}</h1>
+<h1>The count is {$count}</h1>
 
-<p>
-	This page has been open for
-	{$elapsed}
-	{$elapsed === 1 ? 'second' : 'seconds'}
-</p>
+<button on:click={count.increment}>+</button>
+<button on:click={count.decrement}>-</button>
+<button on:click={count.reset}>reset</button>
 
 <!--
 
-  Você pode criar uma loja cujo valor é baseado no valor de uma ou mais outras lojas com derivado. 
-  Com base em nosso exemplo anterior, podemos criar uma loja que deriva o tempo em que a página foi aberta: (linhas 13 a 18 em stores.js);
-
-  É possível derivar uma store de várias stores de entrada, e um valor set explicitamente em vez de retorná-lo 
-  (o que é útil para derivar valores de forma assíncrona). Consulte a referência da API para obter mais informações.
+  Contanto que um objeto implemente corretamente o método de assinatura, ele é um armazenamento.
+  Além disso, vale tudo. É muito fácil, portanto, criar repositórios personalizados com lógica específica do domínio.
 
   CONTEXTUALIZANDO O CÓDIGO:
-    Contexualização do código presente no commit passado. O código atual apenas sofreu algumas alterações
+    Este código é um exemplo de como usar uma loja gravável no Svelte para gerenciar um contador. 
+    O código está dividido em dois arquivos: stores.js e App.svelte.
+
+    - No arquivo stores.js, é criada uma loja gravável chamada count que armazena o valor atual do contador. 
+        A loja é criada usando a função writable do Svelte, que recebe como argumento o valor inicial da loja.
+
+    - A função createCount é usada para criar a loja e retornar um objeto com os métodos subscribe, increment, decrement e reset. 
+        Esses métodos são usados para se inscrever na loja, incrementar, decrementar e redefinir o valor do contador, respectivamente.
+
+    - No arquivo App.svelte, importamos a loja count do arquivo stores.js e usamos seus métodos para gerenciar o contador. 
+        Também usamos a sintaxe $count para acessar o valor atual da loja diretamente no template do componente.
+
+    - Por fim, adicionamos botões ao componente para incrementar, decrementar e redefinir o valor do contador usando os métodos da loja. 
+        Quando um botão é clicado, o método correspondente da loja é chamado e o valor do contador é atualizado.
 
 -->
