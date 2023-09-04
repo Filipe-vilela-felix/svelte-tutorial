5e2578ecaa75fdceec43c80410caa2230a14d935
Filipe Vilela <fvf0092000@gmail.com>
2023-09-04 17:04:06 -0300

    readable-stores

--- src/App.svelte
+++ src/App.svelte
@@ -1,43 +1,47 @@
 <script>
-//import { onDestroy } from 'svelte';
-	import { count } from './stores.js';
-	import Incrementer from './Incrementer.svelte';
-	import Decrementer from './Decrementer.svelte';
-	import Resetter from './Resetter.svelte';
+	import { time } from './stores.js';
 
-//let count_value;
-
-//const unsubscribe = count.subscribe((value) => {
-// count_value = value;
-//});
-
-//onDestroy(unsubscribe);
+	const formatter = new Intl.DateTimeFormat(
+		'en',
+		{
+			hour12: true,
+			hour: 'numeric',
+			minute: '2-digit',
+			second: '2-digit'
+		}
+	);
 </script>
 
-<h1>The count is {$count}</h1>
-
-<Incrementer />
-<Decrementer />
-<Resetter />
+<h1>The time is {formatter.format($time)}</h1>
 
 <!--
 
-  O aplicativo no exemplo anterior funciona, mas há um bug sutil — a loja é assinada, mas nunca cancelada. 
-  Se o componente foi instanciado e destruído muitas vezes, isso resultaria em um vazamento de memória.
+  Nem todas as lojas devem ser gravadas por quem tem uma referência a elas. 
+  Por exemplo, você pode ter uma loja representando a posição do mouse ou a geolocalização do usuário, 
+  e não faz sentido poder definir esses valores de 'fora'. Para esses casos, temos lojas legíveis.
 
-  Chamar um método subscribe retorna uma função de cancelamento de assinatura. (linha 10)
+  Em stores.js, o primeiro argumento para readable é um valor inicial, que pode ser null ou undefined se você ainda não tiver um. 
+  O segundo argumento é uma função start que usa um set callback definido e retorna uma função stop. 
+  A função start é chamada quando a loja recebe seu primeiro assinante; stop é chamado quando o último assinante cancela a assinatura. (linha 2 a 6 em store.js);
 
-  Agora você declarou unsubcribe, mas ainda precisa ser chamado, por exemplo, por meio do gancho do ciclo de vida onDestroy. (linha 2)
 
-  No entanto, ele começa a ficar um pouco clichê, especialmente se seu componente assinar várias lojas. 
-  Em vez disso, o Svelte tem um truque na manga — você pode fazer referência a um valor de loja prefixando o nome da loja com $: (linha 17);
+  CONTEXTUALIZANDO O CÓDIGO:
+    Este código é um exemplo de como usar uma loja legível no Svelte para exibir a hora atual em um componente. 
+    O código está dividido em dois arquivos: App.svelte e stores.js.
 
-  Obs: A assinatura automática só funciona com variáveis de armazenamento declaradas (ou importadas) no escopo de nível superior de um componente.
+    - No arquivo stores.js, é criada uma loja legível chamada time que armazena a hora atual. 
+      A loja é criada usando a função readable do Svelte, que recebe dois argumentos: o valor inicial da loja e uma função de inicialização.
 
-  Obs: Você também não está limitado a usar $count dentro da marcação — você também pode usá-la em qualquer lugar <script> , 
-        como em manipuladores de eventos ou declarações reativas.
+    - A função de inicialização é chamada quando a loja é criada e recebe como argumento uma função set que pode ser usada para atualizar o valor 
+      da loja. Neste caso, usamos um intervalo para atualizar a hora a cada segundo e usamos a função set para atualizar o valor da loja.
 
-  Obs: Presume-se que qualquer nome que comece com $ se refira a um valor de loja. 
-        É efetivamente um caractere reservado — o Svelte impedirá que você declare suas próprias variáveis com um prefixo $.
+    - A função de inicialização também retorna uma função que é chamada quando a loja é desmontada. 
+      Neste caso, usamos essa função para limpar o intervalo que criamos anteriormente.
+
+    - No arquivo App.svelte, importamos a loja time do arquivo stores.js e usamos seu valor para exibir a hora atual no componente. 
+      Também criamos um objeto formatter usando a classe Intl.DateTimeFormat para formatar a hora de acordo com as opções especificadas.
+
+    - Por fim, usamos a loja legível diretamente no template do componente para exibir a hora formatada. 
+      Note que não podemos alterar o valor da loja diretamente, apenas ler seu valor atual.
 
 -->
