42d5bb04fe5fabf5f64b865b21f34adc7044bb73
Filipe Vilela <fvf0092000@gmail.com>
2023-09-04 17:27:34 -0300

    derived-stores

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { time } from './stores.js';
+	import { time, elapsed } from './stores.js';
 
 	const formatter = new Intl.DateTimeFormat(
 		'en',
@@ -14,34 +14,21 @@
 
 <h1>The time is {formatter.format($time)}</h1>
 
+<p>
+	This page has been open for
+	{$elapsed}
+	{$elapsed === 1 ? 'second' : 'seconds'}
+</p>
+
 <!--
 
-  Nem todas as lojas devem ser gravadas por quem tem uma referência a elas. 
-  Por exemplo, você pode ter uma loja representando a posição do mouse ou a geolocalização do usuário, 
-  e não faz sentido poder definir esses valores de 'fora'. Para esses casos, temos lojas legíveis.
+  Você pode criar uma loja cujo valor é baseado no valor de uma ou mais outras lojas com derivado. 
+  Com base em nosso exemplo anterior, podemos criar uma loja que deriva o tempo em que a página foi aberta: (linhas 13 a 18 em stores.js);
 
-  Em stores.js, o primeiro argumento para readable é um valor inicial, que pode ser null ou undefined se você ainda não tiver um. 
-  O segundo argumento é uma função start que usa um set callback definido e retorna uma função stop. 
-  A função start é chamada quando a loja recebe seu primeiro assinante; stop é chamado quando o último assinante cancela a assinatura. (linha 2 a 6 em store.js);
-
+  É possível derivar uma store de várias stores de entrada, e um valor set explicitamente em vez de retorná-lo 
+  (o que é útil para derivar valores de forma assíncrona). Consulte a referência da API para obter mais informações.
 
   CONTEXTUALIZANDO O CÓDIGO:
-    Este código é um exemplo de como usar uma loja legível no Svelte para exibir a hora atual em um componente. 
-    O código está dividido em dois arquivos: App.svelte e stores.js.
-
-    - No arquivo stores.js, é criada uma loja legível chamada time que armazena a hora atual. 
-      A loja é criada usando a função readable do Svelte, que recebe dois argumentos: o valor inicial da loja e uma função de inicialização.
-
-    - A função de inicialização é chamada quando a loja é criada e recebe como argumento uma função set que pode ser usada para atualizar o valor 
-      da loja. Neste caso, usamos um intervalo para atualizar a hora a cada segundo e usamos a função set para atualizar o valor da loja.
-
-    - A função de inicialização também retorna uma função que é chamada quando a loja é desmontada. 
-      Neste caso, usamos essa função para limpar o intervalo que criamos anteriormente.
-
-    - No arquivo App.svelte, importamos a loja time do arquivo stores.js e usamos seu valor para exibir a hora atual no componente. 
-      Também criamos um objeto formatter usando a classe Intl.DateTimeFormat para formatar a hora de acordo com as opções especificadas.
-
-    - Por fim, usamos a loja legível diretamente no template do componente para exibir a hora formatada. 
-      Note que não podemos alterar o valor da loja diretamente, apenas ler seu valor atual.
+    Contexualização do código presente no commit passado. O código atual apenas sofreu algumas alterações
 
 -->
