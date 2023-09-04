4c94f2cbdef069146816caf1fb2d058a54e2523e
Filipe Vilela <fvf0092000@gmail.com>
2023-09-04 20:16:03 -0300

    store-bindings

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,38 @@
 <script>
-    import { count } from "./stores";
+  import { name, greeting } from "./stores";
 </script>
 
-<h1>The count is {$count}</h1>
+<h1>{$greeting}</h1><br>
+<input bind:value={$name} />
 
-<button on:click={count.increment}>+</button>
-<button on:click={count.decrement}>-</button>
-<button on:click={count.reset}>reset</button>
+<button on:click={() => $name += '!'}>
+  Add exclamation mark!
+</button>
 
 <!--
 
-  Contanto que um objeto implemente corretamente o método de assinatura, ele é um armazenamento.
-  Além disso, vale tudo. É muito fácil, portanto, criar repositórios personalizados com lógica específica do domínio.
+  Se um armazenamento for gravável — ou seja, tiver um método definido — você poderá vincular ao seu valor, 
+  assim como poderá vincular ao estado do componente local.
+
+  Também podemos atribuir diretamente para armazenar valores dentro de um componente. (linha 8);
+  A atribuição $name += '!' é equivalente a name.set($name + '!').
 
   CONTEXTUALIZANDO O CÓDIGO:
-    Este código é um exemplo de como usar uma loja gravável no Svelte para gerenciar um contador. 
-    O código está dividido em dois arquivos: stores.js e App.svelte.
+    Este código é um exemplo de como usar lojas graváveis e derivadas no Svelte para gerenciar um nome e uma saudação. 
+    O código está dividido em dois arquivos: App.svelte e stores.js.
 
-    - No arquivo stores.js, é criada uma loja gravável chamada count que armazena o valor atual do contador. 
-        A loja é criada usando a função writable do Svelte, que recebe como argumento o valor inicial da loja.
+    - No arquivo stores.js, são criadas duas lojas: uma loja gravável chamada name que armazena o nome atual e 
+        uma loja derivada chamada greeting que armazena a saudação atual. A loja name é criada usando a função writable do Svelte, 
+        que recebe como argumento o valor inicial da loja. A loja greeting é criada usando a função derived do Svelte, 
+        que recebe como argumentos a loja name e uma função que retorna a saudação baseada no valor atual da loja name.
 
-    - A função createCount é usada para criar a loja e retornar um objeto com os métodos subscribe, increment, decrement e reset. 
-        Esses métodos são usados para se inscrever na loja, incrementar, decrementar e redefinir o valor do contador, respectivamente.
-
-    - No arquivo App.svelte, importamos a loja count do arquivo stores.js e usamos seus métodos para gerenciar o contador. 
-        Também usamos a sintaxe $count para acessar o valor atual da loja diretamente no template do componente.
-
-    - Por fim, adicionamos botões ao componente para incrementar, decrementar e redefinir o valor do contador usando os métodos da loja. 
-        Quando um botão é clicado, o método correspondente da loja é chamado e o valor do contador é atualizado.
+    - No arquivo App.svelte, importamos as lojas name e greeting do arquivo stores.js e usamos seus valores para exibir a saudação atual e 
+        o nome atual no componente. Também adicionamos um campo de entrada ao componente e usamos a sintaxe de vinculação do Svelte 
+        para criar uma conexão bidirecional entre o valor da loja name e o valor do campo de entrada. Isso significa que, 
+        quando o usuário digitar algo no campo de entrada, o valor da loja name será atualizado automaticamente.
+      
+    - Por fim, adicionamos um botão ao componente para adicionar um ponto de exclamação ao valor da loja name. 
+        Quando o botão é clicado, o manipulador de eventos associado ao botão é chamado e o valor da loja name é atualizado adicionando um ponto 
+        de exclamação ao seu final.
 
 -->
