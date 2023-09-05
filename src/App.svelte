93d2715ced697ea4d41c8adbcd1413f471c3d07a
Filipe Vilela <fvf0092000@gmail.com>
2023-09-05 09:15:45 -0300

    springs

--- src/App.svelte
+++ src/App.svelte
@@ -1,71 +1,89 @@
 <script>
-//import { writable } from 'svelte/store';
-  import { tweened } from "svelte/motion";
-  import { cubicOut } from "svelte/easing";
+	// import { writable } from 'svelte/store';
 
-//const progress = writable(0);
+	// let coords = writable({ x: 50, y: 50 });
+	// let size = writable(10);
+  import { spring } from 'svelte/motion';
 
-  const progress = tweened(0, {
-    duration: 400,
-    easing: cubicOut
-  });
+  let coords = spring({ x: 50, y: 50 }, {
+		stiffness: 0.1,
+		damping: 0.25
+	});
+  let size = spring(10);
 </script>
 
-<progress value={$progress} />
+<svg
+	on:mousemove={(e) => {
+		coords.set({ x: e.clientX, y: e.clientY });
+	}}
+	on:mousedown={() => size.set(30)}
+	on:mouseup={() => size.set(10)}
+>
+	<circle
+		cx={$coords.x}
+		cy={$coords.y}
+		r={$size}
+	/>
+</svg>
 
-<button on:click={() => progress.set(0)}>
-  0%
-</button>
+<div class="controls">
+	<label>
+		<h3>stiffness ({coords.stiffness})</h3>
+		<input
+			bind:value={coords.stiffness}
+			type="range"
+			min="0.01"
+			max="1"
+			step="0.01"
+		/>
+	</label>
 
-<button on:click={() => progress.set(0.25)}>
-  25%
-</button>
-
-<button on:click={() => progress.set(0.50)}>
-  50%
-</button>
-
-<button on:click={() => progress.set(0.75)}>
-  75%
-</button>
-
-<button on:click={() => progress.set(1)}>
-  100%
-</button>
+	<label>
+		<h3>damping ({coords.damping})</h3>
+		<input
+			bind:value={coords.damping}
+			type="range"
+			min="0.01"
+			max="1"
+			step="0.01"
+		/>
+	</label>
+</div>
 
 <style>
-  progress {
-    display: block;
-    width: 100%;
-  }
+	svg {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		left: 0;
+		top: 0;
+	}
+
+	circle {
+		fill: #ff3e00;
+	}
+
+	.controls {
+		position: absolute;
+		top: 1em;
+		right: 1em;
+		width: 200px;
+		user-select: none;
+	}
+
+	.controls input {
+		width: 100%;
+	}
 </style>
 
 <!--
 
-  A primeira técnica avançada é o movimento (motions).default
+  A função spring (mola) é uma alternativa à interpolação que geralmente funciona melhor para valores que estão mudando com frequência.
 
-  Definir valores e assistir à atualização do DOM automaticamente é legal. Sabe o que é ainda mais legal? Interpolação desses valores. 
-  O Svelte inclui ferramentas para ajudá-lo a criar interfaces de usuário lisas que usam animação para comunicar alterações.
+  Neste exemplo, temos duas lojas — uma representando as coordenadas do círculo e outra representando seu tamanho. 
+  Ao invés de writable (linhas 2 a 5), vamos convertê-los em springs (linhas 6 a 9).
 
-  Vamos começar alterando o repositório de progresso (linhas 2 e 6) para um interpolado (linhas 3 e 8 );
-
-  Clicar nos botões faz com que a barra de progresso seja animada para seu novo valor. 
-  É um pouco robótico e insatisfatório embora. Precisamos adicionar uma função de flexibilização. (linhas 4 e 10);
-
-  Obs: O módulo svelte/easing contém as equações de atenuação de Penner, 
-        ou você pode fornecer sua própria função p => t onde p e t são ambos valores entre 0 e 1.
-  
-  O conjunto completo de opções disponíveis para interpolados:
-    - delay — milissegundos antes do início da interpolação.
-    - durartion — A duração da interpolação em milissegundos ou uma função (de, para) => milissegundos que permite 
-        (por exemplo) especificar interpolações mais longas para alterações maiores no valor.
-    - easing — a função p => t.
-    - interpolate — uma função de valor personalizada (de, para) => t => para interpolação entre valores arbitrários. 
-        Por padrão, o Svelte interpolará entre números, datas e matrizes e objetos de forma idêntica 
-          (desde que contenham apenas números e datas ou outras matrizes e objetos válidos). 
-        Se você quiser interpolar (por exemplo) cadeias de cores ou matrizes de transformação, forneça um interpolador personalizado.
-  
-  Você também pode passar essas opções para progress.set e progress.update como um segundo argumento, 
-  caso em que elas substituirão os padrões. Os métodos set e update retornam uma promessa que é resolvida quando a interpolação é concluída.
+  Ambas as molas têm valores padrão de stiffness (rigidez) e damping (armotecimento), que controlam a mola, bem… a elasticidade. 
+  Podemos especificar nossos próprios valores iniciais. (linhas 9 e 10);
 
 -->
